e368f1700e387be26aedb87a5cad4bd43114e15b
Yuriy Skrypnyk <skrypnyk@hotmail.it>
2018-06-03 22:44:49 +0200

    Update count_word.py

--- count_word.py
+++ count_word.py
@@ -9,3 +9,7 @@ L = sorted(cnt.items(), key = lambda x: x[1], reverse = True)
 a = L[0][0], L[0][1]
 with open('solution.txt', 'w') as word:
     word.write(str(a))
+
+#in two strings without sort   
+from collections import Counter
+print('\n'.join(f'{w} {n}' for w, n in Counter(open('dataset_3363_3.txt').read().lower().split()).most_common()), file=open('out', 'w'))
