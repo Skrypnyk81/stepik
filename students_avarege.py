9036d41cb43605827faee7cab17c2e74fc70587a
Yuriy Skrypnyk <skrypnyk@hotmail.it>
2018-06-07 10:54:25 +0200

    Update students_avarege.py

--- students_avarege.py
+++ students_avarege.py
@@ -17,3 +17,14 @@ tot_avr = str(avr_P) + ' ' + str(avr_S) + ' ' + str(avr_T)
 with open('solution_4.txt', 'w') as word:
     for i in C: word.write(str(i) + '\n')
     word.write(str(tot_avr))
+
+    
+#secondo codice accetato dal stepik
+with open('dataset_3363_4.txt') as f:
+    strings = [s.rstrip() for s in f.readlines()]
+otsenki = [s.split(';')[1:] for s in strings]
+
+sr_mat = sum([int(x[0]) for x in otsenki]) / len(otsenki)
+sr_fiz = sum([int(x[1]) for x in otsenki]) / len(otsenki)
+sr_rus = sum([int(x[2]) for x in otsenki]) / len(otsenki)
+print(sr_mat, sr_fiz, sr_rus)
